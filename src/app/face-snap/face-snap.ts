f2bec25a9c5bc5286e7f645895f8becbb939af9c
abdeljamil <abdeljamil610@gmail.com>
2026-04-23 23:12:41 +0100

    ajout de bouton onSnap ecout d'événement sur la vue

--- src/app/face-snap/face-snap.ts
+++ src/app/face-snap/face-snap.ts
@@ -21,7 +21,7 @@ export class FaceSnapComponent implements OnInit {
     this.description = 'Mon meilleur ami depuis tout petit !';
     this.createdDate = new Date();
     this.snaps = 6;
-    this.imageUrl = 'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-789807_1280.jpg';
+    this.imageUrl = 'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg';
     this.snapButtonText = 'Oh Snap !';
     this.userHasSnapped = false;
   }
